c14548f585295cb62c2cc1d0e3fa1e9ba7defaee
namwamba <namwamba.eduacc@gmail.com>
2026-07-26 15:36:03 +0300

    first commit

---
+++ cmd/api/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello from Go")
+}
+func main() {
+	http.HandleFunc("/", helloHandler)
+
+	log.Println("Starting server on :8080")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
